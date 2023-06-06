9241e358c420e250ff149e91ecff7725c417d9ee
Sayantan <sayantan358@gmail.com>
2023-06-06 14:09:39 +0530

    added express Q6 jwt

--- Assignment Projects/Express Q6. JWT Authentication/Server/config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  JWT_SECRET : "0PGn7BmI4o8JjxNPHKgd8kGxYYYjHlP4SOB+3IApRX0=",
-  EMAIL: "madaline.walsh53@ethereal.email",
-  PASSWORD : "b1nPR4tEcZwjWgj18U",
-  ATLAS_URI: "mongodb+srv://sayantan23198:vQ3GCLw58jyRvMPu@cluster0.fidcrt4.mongodb.net/"
-}
